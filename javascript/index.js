421bffff966b957ce6662bd5315491cd71a089b5
katrina864 <60376415+katrina864@users.noreply.github.com>
2020-02-19 02:24:12 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,18 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
 
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
 function openPage(pageName, elmnt, color) {
   // Hide all elements with class="tabcontent" by default */
   var i, tabcontent, tablinks;
